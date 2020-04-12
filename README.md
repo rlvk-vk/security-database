1cda8d3bf3d09f31c48c9d10ca9872e81b5b9672
Robin Louis van Komen <robin@vkmedia.dk>
2020-04-12 14:09:40 +0200

    add initial search logic

--- README.md
+++ README.md
@@ -1,28 +1,3 @@
-# React Redux TypeScript Template
+# Security Database
 
-## How to use this template
-
-1. Clone this repository
-2. Delete the `.git` folder
-3. Run `git init` in your terminal
-4. Run `git add .` in your terminal
-5. Run `git commit -m "initial commit"` in your terminal
-
-## What does the template include
-
-### Packages
-
--   Axios
--   Immer
--   Node Sass
--   React
--   Redux
--   Redux Logger
--   Redux Thunk
--   Redux Devtools
--   Typescript
-
-### Other
-
--   Prettier
--   TsLint
+This is a simple prototype website
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ReactComponent as SearchIcon } from '../icons/search.svg'
 import styles from './App.module.scss'
 import Categories from './categories/Categories'
 import logo from './emerson-logo.png'
-import { ReactComponent as SearchIcon } from '../icons/search.svg'
 
 const App: React.FC = () => {
+	const [searchword, setSearchword] = useState('')
+
+	const submitSearch = (event: any) => {
+		if (event.key === 'Enter' || event.target.nodeName === 'svg') {
+			console.log(searchword)
+		}
+	}
+
+	const handleSearch = (event: any) => {
+		setSearchword(event.target.value)
+	}
+
 	return (
 		<div className={styles.app}>
 			<img className={styles.logo} src={logo} alt="emerson-logo"/>
 			<h1>Security Database</h1>
 
 			<div className={styles.search}>
-				<input type="text" placeholder="Search activity..."/>
-				<SearchIcon />
+				<input type="text"
+					   placeholder="Search activity..."
+					   value={searchword}
+					   onChange={handleSearch}
+					   onKeyPress={submitSearch}
+				/>
+				<SearchIcon onClick={submitSearch}/>
 			</div>
 
 			<Categories/>
